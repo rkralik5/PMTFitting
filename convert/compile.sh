6e2e192808e8c256cceba49fc43d60ed71f95d5f
Mahdi Taani <22234246+mahditaani@users.noreply.github.com>
2021-09-16 17:42:05 +0100

    Modified compile script

--- convert/compile.sh
+++ convert/compile.sh
@@ -1,5 +1,5 @@
-#export BOOST=/mnt/c/Users/ABagO/Desktop/XMLHandling/boost_1_74_0
+#export BOOST=/path/to/boost
 
 g++  -O2 -Wall -fPIC -pthread -std=c++11 -m64 -I$BOOST -I$ROOTSYS/include -c main.cpp
 g++  -O2 -m64 -std=c++11 main.o -lm -L$ROOTSYS/lib -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lMultiProc -pthread -lm -ldl -rdynamic -o waveconvert
-
+rm main.o
