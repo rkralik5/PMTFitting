c373c0aca48b44ccc3f5e11eb14bda445d1b2c46
Robert Kralik <kralikrobo@gmail.com>
2025-08-22 12:03:00 +0100

    Removing obsolete converting scripts

--- convert/compile_hannah.sh
+++ convert/compile_hannah.sh
@@ -2,13 +2,13 @@ export BOOST=/usr/local/opt/boost  # Boost path
 export ROOTSYS=/usr/local/opt/root  # ROOT path
 export DYLD_LIBRARY_PATH=$ROOTSYS/lib:$DYLD_LIBRARY_PATH
 
-g++ -O2 -Wall -fPIC -pthread -std=c++20 -m64 \
+g++ -O2 -Wall -fPIC -pthread -std=c++17 -m64 \
     -I$BOOST/include \
     -I$(root-config --incdir) \
     -c main.cpp 
 
 # Link the object file with ROOT and Boost libraries
-g++ -O2 -m64 -std=c++20 main.o \
+g++ -O2 -m64 -std=c++17 main.o \
     -L$ROOTSYS/lib/root \
     -lCore -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint \
     -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lMultiProc \
